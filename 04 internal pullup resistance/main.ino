ebc3c5fb069053b82e2cb9f328cd650564f0602b
Mahmoud Amr <38290949+mahmoudamr1205@users.noreply.github.com>
2019-09-28 17:04:48 +0200

    Update main.ino

--- 04 internal pullup resistance/main.ino
+++ 04 internal pullup resistance/main.ino
@@ -10,6 +10,17 @@ digitalWrite(button,HIGH); //to activited internal pullup resistor arduino
 }
 
 void loop() {
+  /*
+  output  0
+  x=!digitalRead(6);
+ y=!(digitalRead(7));
+ 
+ output 1
+  x=digitalRead(6);
+ y=(digitalRead(7));
+ 
+ 
+ */
   
  if( digitalRead(button)==0){
   
